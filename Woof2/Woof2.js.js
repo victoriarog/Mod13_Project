bfd33a46d809c1159d64f2b660e7e04985a31e22
Victoria Rogatinskaya <vicrogatinskaya@gmail.com>
2021-07-30 02:44:59 +0200

    Woof2

---
+++ Woof2/Woof2.js.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  autoplay: false,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
+
+window.onscroll = function() {
+  var el = document.getElementsByClassName('header')[0];
+  var className = 'small';
+  if (el.classList) {
+    if (window.scrollY > 10)
+      el.classList.add(className);
+    else
+      el.classList.remove(className);
+  }
+};
