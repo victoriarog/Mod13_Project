ccbb6eb061326781a43df1b633e62906d7bfe9e4
Victoria Rogatinskaya <vicrogatinskaya@gmail.com>
2021-08-02 02:57:23 +0200

    maybbe this is the end

--- Woof2.js
+++ Woof2.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
 $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
@@ -6,7 +9,7 @@ $('.owl-carousel').owlCarousel({
     "<i class='fa fa-caret-left'></i>",
     "<i class='fa fa-caret-right'></i>"
   ],
-  autoplay: false,
+  autoplay: true,
   autoplayHoverPause: true,
   responsive: {
     0: {
@@ -19,9 +22,9 @@ $('.owl-carousel').owlCarousel({
       items: 5
     }
   }
-})
+});
 
-window.onscroll = function() {
+window.onscroll = function () {
   var el = document.getElementsByClassName('header')[0];
   var className = 'small';
   if (el.classList) {
